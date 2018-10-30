2d5cc6e180ad16a4e07d0e4d866b478368f4f3c5
Jeb Rosen <jeb.rosen@systemsbiology.org>
2018-10-30 10:41:40 -0700

    Implement annotated taxa query.

--- service/TaxaService.js
+++ service/TaxaService.js
@@ -2,8 +2,11 @@
 
 const Sequelize = require("sequelize");
 const conn = require("../databases.js").dfam;
+const assembly = require("../models/assembly.js")(conn, Sequelize);
 const ncbiTaxonomyNames = require("../models/ncbi_taxdb_names.js")(conn, Sequelize);
 const ncbiTaxonomyNodes = require("../models/ncbi_taxdb_nodes.js")(conn, Sequelize);
+
+ncbiTaxonomyNames.belongsTo(assembly, { foreignKey: 'tax_id', targetKey: 'dfam_taxdb_tax_id' });
 
 /**
  * Query the local copy of the NCBI taxonomy database
@@ -12,15 +15,36 @@ const ncbiTaxonomyNodes = require("../models/ncbi_taxdb_nodes.js")(conn, Sequeli
  * limit Integer Only return up to a maximum number of hits
  * returns taxaResponse
  **/
-exports.readTaxa = function(name,limit) {
-  return ncbiTaxonomyNames.findAll({
-    where: {
-      name_class: "scientific name",
-      name_txt: { [Sequelize.Op.like]: name + "%" },
-    },
-    limit: limit || 20,
-  }).then(function(results) {
-    return { "taxa": results.map(r => ({ "species_name": r.name_txt })) };
-  });
+exports.readTaxa = function(name,limit,annotated) {
+  if (annotated) {
+    return ncbiTaxonomyNames.findAll({
+      where: {
+        name_class: "scientific name",
+        name_txt: { [Sequelize.Op.like]: name + "%" },
+      },
+      include: {
+        model: assembly,
+        where: {
+          schema_name: { [Sequelize.Op.like]: "_%" },
+        }
+      },
+      order: [
+        [ { model: assembly}, 'display_order' ],
+      ],
+      limit: limit || 20,
+    }).then(function(results) {
+      return { "taxa": results.map(r => ({ "species_name": r.name_txt })) };
+    });
+  } else {
+    return ncbiTaxonomyNames.findAll({
+      where: {
+        name_class: "scientific name",
+        name_txt: { [Sequelize.Op.like]: name + "%" },
+      },
+      limit: limit || 20,
+    }).then(function(results) {
+      return { "taxa": results.map(r => ({ "species_name": r.name_txt })) };
+    });
+  }
 }
 
