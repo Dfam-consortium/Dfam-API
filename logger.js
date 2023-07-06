4bad20a7a623105ce46cbc607e55bb05f204ee2f
Robert Hubley <rhubley@systemsbiology.org>
2023-07-06 11:15:05 -0700

    - configuring logging

--- logger.js
+++ logger.js
@@ -8,7 +8,6 @@ const logger = createLogger({
   ),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new transports.Console(),
     new transports.File({ filename: 'error.log', level: 'error', timestamp: true }),
     new transports.File({ filename: 'combined.log', timestamp: true }),
   ],
