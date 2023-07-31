cff8d6c4bbc29db2461894f8ea5282b81118a3dd
asgray <antgray89@gmail.com>
2023-07-31 16:58:40 -0700

    fixed Assemblies and Classifications

--- services/AssembliesService.js
+++ services/AssembliesService.js
@@ -5,19 +5,27 @@ const { Op } = require("sequelize");
 
 /**
 * Retrieve a list of all genome assemblies in Dfam that have annotations.
-* Retrieve a list of all genome assemblies in Dfam that have annotations.
 *
 * returns List
 * */
 const readAssemblies = () => new Promise(
   async (resolve, reject) => {
     try {
+
       const assemblies = await dfam.assemblyModel.findAll({
         where: {
-          schema_name: {[Op.not]: null}
-        }
+          display_order: {[Op.gt]: 0 }
+        },
+        include: [dfam.dfamTaxdbModel],
+        order_by: ["display_order"]
       })
-      resolve(Service.successResponse(assemblies));
+
+      resolve(Service.successResponse(assemblies.map(
+        (row) => {
+          return { id: row.name, name: row.dfam_taxdb.scientific_name }
+        }
+      )));
+
     } catch (e) {
       reject(Service.rejectResponse(
         e.message || 'Invalid input',
