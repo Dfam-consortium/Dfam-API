12712ef7c1df3841033cf2e181c3ef14a8f95bdf
asgray <antgray89@gmail.com>
2023-08-24 12:05:22 -0700

    all endpoints working

--- services/AlignmentService.js
+++ services/AlignmentService.js
@@ -12,13 +12,14 @@ const zlib = require("zlib");
 const Service = require('./Service');
 
 
-const formatAlignment = async ({ seqID, ordStart, ordEnd, nhmmer_out }) => {
+const formatAlignment = async ( seqID, ordStart, ordEnd, nhmmer_out ) => {
+  try {
+
   let alignRec = {};
 
   // winston.debug(`nhmmer_out: ${nhmmer_out}`);
   let lines = nhmmer_out.split(/[\r\n]/);
   let lineIndex = 0;
-
   function advanceAndMatch(regex) {
     let match;
     while (!match && lineIndex < lines.length) {
@@ -92,56 +93,63 @@ const formatAlignment = async ({ seqID, ordStart, ordEnd, nhmmer_out }) => {
   };
 
   return alignRec;
+} catch (err) {
+  console.log("formatAlignment Failed "+err)
+}
 }
 
-const reAlignAnnotationHMM = async (twoBitFile, seqID, seqName, startPos, endPos, hmmData) => {
+async function reAlignAnnotationHMM(twoBitFile, seqID, seqName, startPos, endPos, hmmData) {
+
+  const [seqFile, hmmFile] = await Promise.all([
+    tmpFileAsync({ detachDescriptor: true }),
+    tmpFileAsync({ detachDescriptor: true }),
+  ]);
+
+  // Save HMM data to file
+  const writeHmmFile = promisify(fs.writeFile)(hmmFile.fd, hmmData, null).then(function() {
+    return promisify(fs.close)(hmmFile.fd);
+  }).catch(function(err) {
+    throw new Error('Error saving hmm data to a temporary file.' + err);
+  });
+
+  var ordStart = startPos;
+  var ordEnd = endPos;
+  if (ordEnd < ordStart)
+  {
+    ordStart = endPos;
+    ordEnd = startPos;
+  }
+
+  const twoBitToFa = path.join(config.ucsc_utils_bin, 'twoBitToFa');
+  const search = twoBitFile + ':' + seqID + ':' + (ordStart-1) + '-' + ordEnd;
+
+  // Grab sequence data from twoBit format
+  const writeFastaFile = execFileAsync(twoBitToFa, [search, 'stdout']).then(function(fasta) {
+    return promisify(fs.writeFile)(seqFile.fd, Buffer.from(fasta.stdout), null);
+  }).then(function() {
+    return promisify(fs.close)(seqFile.fd);
+  }).catch(function(err) {
+    throw new Error('Error saving sequence data to a temporary file.' + err);
+  });
+
   try {
-    const [seqFile, hmmFile] = await Promise.all([
-      tmpFileAsync({ detachDescriptor: true }),
-      tmpFileAsync({ detachDescriptor: true }),
-    ]);
+    await Promise.all([writeHmmFile, writeFastaFile]);
 
-    const writeHmmFile = fs.writeFile(hmmFile.fd, hmmData, (err) =>  err)
-    fs.close(hmmFile.fd);
+    // Do the search
+    const nhmmer = path.join(config.hmmer_bin_dir, 'nhmmer');
 
-    let ordStart = startPos;
-    let ordEnd = endPos;
-    if (ordEnd < ordStart){ 
-      ordStart = endPos; 
-      ordEnd = startPos; 
-    }
-    const twoBitToFa = path.join(config.ucsc_utils_bin, 'twoBitToFa');
-    const search = twoBitFile + ':' + seqID + ':' + (ordStart-1) + '-' + ordEnd;
+    // HACK: (JR) Passing '-T 0' to force nhmmer to show all results regardless of score or e-value.
+    // TODO: (JR) A region might match a model more than once. The "best" match within the
+    //       region will be used here, which might not be the right one.
+    const nhmmer_out = await execFileAsync(nhmmer, ['--max', '-T', '0', '--notextw', hmmFile.path, seqFile.path]);
 
-    // Grab sequence data from twoBit format
-    const writeFastaFile = await execFileAsync(twoBitToFa, [search, 'stdout'])
+    return formatAlignment(seqName, ordStart, ordEnd, nhmmer_out.stdout, startPos);
 
-    fs.writeFile(seqFile.fd, Buffer.from(writeFastaFile.stdout), (err) => err);
-    fs.close(seqFile.fd);
-
-    try {
-      await Promise.all([writeHmmFile, writeFastaFile]);
-  
-      // Do the search
-      const nhmmer = path.join(config.hmmer_bin_dir, 'nhmmer');
-      // HACK: (JR) Passing '-T 0' to force nhmmer to show all results regardless of score or e-value.
-      // TODO: (JR) A region might match a model more than once. The "best" match within the
-      //       region will be used here, which might not be the right one.
-      const nhmmer_out = await execFileAsync(nhmmer, ['--max', '-T', '0', '--notextw', hmmFile.path, seqFile.path]);
-  
-      return formatAlignment(seqName, ordStart, ordEnd, nhmmer_out.stdout, startPos);
-    } finally {
-      hmmFile.cleanup();
-      seqFile.cleanup();
-    }
-
-  } catch (e) {
-    let message = e.message || 'Invalid input'
-    let status = e.status || 405
-    return {message, status}
+  } finally {
+    hmmFile.cleanup();
+    seqFile.cleanup();
   }
 }
-//)
 
 
 /**
@@ -155,13 +163,13 @@ const reAlignAnnotationHMM = async (twoBitFile, seqID, seqName, startPos, endPos
 * family String The family to align against.
 * returns alignmentResponse
 * */
-const readAlignment = ({ assembly, chrom, start, end, family }) => new Promise(
+const readAlignment = async ({ assembly, chrom, start, end, family }) => new Promise(
   async (resolve, reject) => {
     try {
-
       if (Math.abs(end-start) > 30000) {
-        resolve(Service.successResponse({ message: "Requested range is too long." },
-        400
+        return Promise.resolve(new APIResponse(
+          { message: "Requested range is too long." },
+          400
         ));
       }
 
@@ -169,45 +177,39 @@ const readAlignment = ({ assembly, chrom, start, end, family }) => new Promise(
         where: { name: assembly },
         attributes: ['schema_name'],
       });
-      
-      if (!assembly_rec) { resolve(Service.successResponse({}, 404)); }
 
-      const models = await getModels_Assembly(assembly_rec.schema_name);
+      if (!assembly_rec) {reject(Service.rejectResponse("Assembly Not Found", 404));}
+
+      const models = getModels_Assembly(assembly_rec.schema_name);
 
       const sequence = await models.sequenceModel.findOne({
         attributes: ['accession'],
         where: { id: chrom }
       });
 
-      if (!sequence) { resolve(Service.successResponse({}, 404)); }
+      if (!sequence) {reject(Service.rejectResponse("Sequence Not Found", 404));}
 
       const model = await dfam.hmmModelDataModel.findOne({
-        where: { family_id: '4150' } // TODO FIX
-        // attributes: [ "hmm" ],
-        // include: [ { model: dfam.familyModel, where: { accession: family }, attributes: [] } ],
+        attributes: [ "hmm" ],
+        include: [ { model: dfam.familyModel, where: { accession: family }, attributes: [] } ],
       });
-      
-      if (!model) { resolve(Service.successResponse({}, 404)); }
 
       const hmm_data = await promisify(zlib.gunzip)(model.hmm);
 
       const twoBitFile = path.join(config.dfam_warehouse_dir,
-        "ref-genomes", assembly, "dfamseq.mask.2bit"
-      );
+        "ref-genomes", assembly, "dfamseq.mask.2bit");
 
-      const re_align = reAlignAnnotationHMM(twoBitFile, sequence.accession, chrom, start, end, hmm_data)
+        resolve(Service.successResponse(reAlignAnnotationHMM(twoBitFile, sequence.accession, chrom, start, end, hmm_data), 200));
 
-      resolve(Service.successResponse(re_align, 200));
-
-    } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
-    }
-  },
-);
+  } catch (e) {
+    reject(Service.rejectResponse(
+      e.message || 'Invalid input',
+      e.status || 405,
+    ));
+  }
+});
 
 module.exports = {
   readAlignment,
+  formatAlignment
 };
