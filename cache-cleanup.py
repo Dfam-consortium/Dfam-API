d9d9dd5f599b1d3867602ba7ddaeb584e730ebd5
asgray <antgray89@gmail.com>
2024-01-03 13:00:35 -0800

    update to cache cleanup

--- cache-cleanup.py
+++ cache-cleanup.py
@@ -1,12 +1,10 @@
 import os
 from datetime import datetime, timedelta
 import json
- 
-current_directory = os.path.dirname(os.path.abspath(__file__))
-# dfweb Config file
-config_dir = current_directory + '/dfam.conf'
-# Dev config file
-# config_dir = current_directory + '/../Conf/dfam-rel.conf'
+import logging
+
+# current_directory = os.path.dirname(os.path.abspath(__file__))
+config_dir = '/local/usrlocal/www/Dfam/Conf/dfam.conf'
 
 with open(config_dir) as f:
     config = json.load(f)
@@ -15,9 +13,24 @@ CACHE_DIR = config['dfamdequeuer']['result_store'] + '/browse-cache/'
 NOW = datetime.now()
 DELTA = timedelta(days=10)
 
+logging.basicConfig(filename='/webresults/cache-cleanup.log',
+        filemode='a',
+        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+        datefmt='%H:%M:%S',
+        level=logging.INFO
+)
+logging.info('Running Cache Cleanup')
+
+logger = logging.getLogger('Cache-Cleanup')
+
 for file in os.listdir(CACHE_DIR):
     file_path = CACHE_DIR + file
     atime = datetime.fromtimestamp(os.stat(file_path).st_atime, tz = None)
     elapsed_time = NOW - atime
+    removed = 'Retained'
     if elapsed_time > DELTA:
+        removed = 'Removed'
         os.remove(file_path)
+
+    logger.info(f'{file} last accessed on {atime} - {removed}')
+                                                                        
