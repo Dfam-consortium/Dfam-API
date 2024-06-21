3aef552e70357ba1c8666c0992e9d126a88c8112
asgray <antgray89@gmail.com>
2024-06-21 15:39:09 -0700

    log parser

---
+++ server/collectLogs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+servers=("dfweb@dfweb1" "dfweb@dfweb2");
+cutoffDate='2024-06-18';
+downloadTime=$(date +%Y-%m-%d)-Export
+
+echo Downloading Logs Newer Than $cutoffDate
+for server in ${servers[@]}; do
+    echo Downloading From $server
+    # finds all /usr/local/www/Dfam/<epoch>/Dfam-API.log files newer than cutoff and concatenates them to a single log file
+    ssh $server find /usr/local/www/Dfam/ -maxdepth 2 -regex .*\/[0-9]*\/Dfam-API.log -type f -newermt $cutoffDate -exec cat {} + >> $downloadTime.log
+done
+
+echo Logs Downloaded To $downloadTime.log
+echo Converting The Log File To CSV
+python3 ./logToCSV.py -i $downloadTime.log -o $downloadTime.csv
