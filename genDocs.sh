fe7e286f56f5bfd87d50c99926e2648c375e8af2
asgray <antgray89@gmail.com>
2025-01-03 16:31:02 -0800

    api docs baseurl fix

---
+++ genDocs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+input_file="api/openapi.yaml"
+output_file="api/openapi-with-prefix.yaml"
+
+old_url="https://dfam.org/"
+new_url="https://dfam.org/api"
+
+sed "s|- url: $old_url|- url: $new_url|g" $input_file > $output_file
+
+npx redocly build-docs "$output_file"
+
+if [ $? -ne 0 ]; then
+  echo "Error occurred during Redocly build."
+  exit 1
+fi
+
+echo "Redocly documentation built successfully."
+
+rm $output_file
